8942a2a1678a4a02ff64e1bb3b5dacbbf697695a
Leo <leodahal4@gmail.com>
2020-12-24 20:03:07 +0545

    added ranjan sir

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -9,4 +9,18 @@ set -e -u
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
+usermod -s /usr/bin/bash root
+cp -aT /etc/skel/ /root/
+useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash bhole
+# chmod 700 /root
+chown -R bhole:users /home/bhole
+
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
+sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+
+systemctl enable pacman-init.service choose-mirror.service 
+systemctl set-default multi-user.target
